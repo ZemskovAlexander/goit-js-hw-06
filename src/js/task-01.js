78c914dea040dc4e695367a47d4e96952af05682
ZemskovAlexander <zemskov25121992@gmail.com>
2022-07-22 00:57:54 +0300

    hard start

---
+++ src/js/task-01.js
@@ -0,0 +1,20 @@
+const itemEl = document.querySelectorAll('.item');
+console.log("Number of categories: ",itemEl.length);
+
+const liEl = document.querySelectorAll('h2');
+console.log("Category: ",liEl[0].textContent);
+
+const animalsEl = document.querySelectorAll('ul');
+console.log("Elements: ",animalsEl.length);
+
+const prodEl = document.querySelectorAll('h2');
+console.log("Category: ",prodEl[1].textContent);
+
+const prodUlEl = document.querySelectorAll('ul');
+console.log("Elements: ",prodUlEl[2].textContent);
+
+const tehEl = document.querySelectorAll('h2');
+console.log("Category: ",tehEl[2].textContent);
+
+const tehUlEl = document.querySelectorAll('ul');
+console.log("Elements: ",prodUlEl[3].textContent);
