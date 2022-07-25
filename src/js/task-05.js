815c484a18f3542c65991c76263f533d12becba7
ZemskovAlexander <zemskov25121992@gmail.com>
2022-07-25 22:40:27 +0300

    fix 5 task

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,6 +1,6 @@
 const refs = {
   input: document.querySelector("#name-input"),
-  nameLabel: document.querySelector("#name-output  > span"),
+  nameLabel: document.querySelector("#name-output"),
 };
 
 refs.input.addEventListener("input", onInputChange);
