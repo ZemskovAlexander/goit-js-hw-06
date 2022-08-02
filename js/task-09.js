d9263de5ecf403b34aa1bf1662b54e49682257f6
ZemskovAlexander <zemskov25121992@gmail.com>
2022-08-02 17:07:06 +0300

    fix

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const singleBtn = document.querySelector("body");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+  return (singleBtn.style.backgroundColor = getRandomHexColor());
+};
+
+singleBtn.addEventListener("click", handleClick);
