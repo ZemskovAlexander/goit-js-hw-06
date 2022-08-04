df587e49e17e6dc66ff1faf4ec1c50f33e7448dc
ZemskovAlexander <zemskov25121992@gmail.com>
2022-08-04 22:24:10 +0300

    fin fix

--- js/task-05.js
+++ js/task-05.js
@@ -1,20 +1,12 @@
 const refs = {
   input: document.querySelector("#name-input"),
   nameLabel: document.querySelector("#name-output"),
-  spanText: document.querySelector("span")
+  spanText: document.querySelector("span"),
 };
 
 refs.input.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
   console.log(event.currentTarget.value);
-  refs.nameLabel.textContent = event.currentTarget.value;
-  
-   if ((refs.nameLabel.textContent = "")) {
-     return refs.nameLabel.textContent.innerHTML(
-       "beforebegin",
-       `<h1>Hello, <span id="name-output">Anonymous</span>!</h1>`
-     );
-   }
- 
-  }
+  refs.nameLabel.textContent = event.currentTarget.value || "Anonymous";
+}
